6f07812eb9df52b85bf4731e1977234befdb18c8
kmadhok <72879711+kmadhok@users.noreply.github.com>
2021-03-04 16:54:20 -0600

    Add files via upload

---
+++ 02-Templates - Dict/Section3Part2.py
@@ -0,0 +1,11 @@
+Snacks = list()
+Snack = dict()
+Snack['snack']="Nestle's Crunch"
+Snack['calories'] = 101
+Snacks.append(Snack)
+Snack=dict()
+print(Snacks)
+Snack['snack']="KitKat"
+Snack['calories'] = 202
+Snacks.append(Snack)
+print(Snacks)
